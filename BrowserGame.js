9b13c49f7239af45aa079a2ed6dd4a2898b37f83
Ben Gilley <bengilley5@gmail.com>
2023-10-02 21:39:47 -0400

    milestone1

---
+++ BrowserGame.js
@@ -0,0 +1,69 @@
+const questionContainer = document.getElementById('question-container');
+const questionText = document.getElementById('question');
+const choicesList = document.getElementById('choices');
+const scoreText = document.getElementById('score');
+const nextButton = document.getElementById('next-button');
+
+const questions = [
+    {
+        question: 'What is the capital of France?',
+        choices: ['London', 'Berlin', 'Paris', 'Madrid'],
+        correctAnswer: 'Paris'
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        choices: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
+        correctAnswer: 'Mars'
+    },
+    {
+        question: 'Who wrote the play "Romeo and Juliet"?',
+        choices: ['William Shakespeare', 'Charles Dickens', 'Jane Austen', 'George Orwell'],
+        correctAnswer: 'William Shakespeare'
+    }
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function displayQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    questionText.textContent = currentQuestion.question;
+    
+    choicesList.innerHTML = '';
+
+    currentQuestion.choices.forEach((choice, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = choice;
+        listItem.addEventListener('click', () => checkAnswer(choice));
+        choicesList.appendChild(listItem);
+    });
+}
+
+function checkAnswer(selectedChoice) {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedChoice === currentQuestion.correctAnswer) {
+        score++;
+    }
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+    } else {
+        endGame();
+    }
+}
+
+function endGame() {
+    questionContainer.style.display = 'none';
+    nextButton.style.display = 'none';
+    scoreText.textContent = `Final Score: ${score} out of ${questions.length}`;
+}
+
+nextButton.addEventListener('click', () => {
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+    } else {
+        endGame();
+    }
+});
+
+displayQuestion();
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Front-End-Milestone-Project
+
+This is the first of 3 Milestone projects as a part of the North Carolina State Software Development Bootcamp program. The objectives of the project are to create a functional
+browser game using HTML, CSS, and JavaScript. 
+
+I chose to make a trivia game, with inspiration taken from Jeopardy.
